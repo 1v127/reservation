446a66f5ee26b4755d334805c2f75f1c27efa203
vida <sanaeivida@gmail.com>
2021-03-21 23:43:13 +0330

    .

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'doctor',
-    'guestuser',
-    'normaluser',
+    'guests',
+    'normal',
 ]
 
 MIDDLEWARE = [
